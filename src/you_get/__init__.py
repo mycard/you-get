fc76ba3d071876a0fdc87b87676fddcd36820bc6
weijian.guo <weijian.guo@gameloft.com>
2015-10-13 17:00:39 +0800

    - modified you-get to make it a library

--- src/you_get/__init__.py
+++ src/you_get/__init__.py
@@ -4,15 +4,13 @@
 import sys
 
 if sys.version_info[0] == 3:
-    #from .extractor import Extractor, VideoExtractor
-    #from .util import log
-
+    from .extractor import Extractor, VideoExtractor
+    from .util import log
     from .__main__ import *
-
-    #from .common import *
-    #from .version import *
-    #from .cli_wrapper import *
-    #from .extractor import *
+    from .common import *
+    from .version import *
+    from .cli_wrapper import *
+    from .extractor import *
 else:
     # Don't import anything.
     pass
